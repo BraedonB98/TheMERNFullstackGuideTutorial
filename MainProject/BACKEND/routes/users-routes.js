2dbc6b8da1f8896134d1d9b5879febb09389e536
braedonb98 <brbe3574@colorado.edu>
2022-01-04 14:11:41 -0700

    just need to link the back end and front end, then also need to add it for places

--- MainProject/BACKEND/routes/users-routes.js
+++ MainProject/BACKEND/routes/users-routes.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const { check } = require('express-validator')
+const { check } = require('express-validator');
+const req = require('express/lib/request');
 
 const usersController = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
-router.post('/signup',
+router.post('/signup', fileUpload.single('image'),
 [check('email').normalizeEmail().isEmail() , check('password').isLength({min:5}) , check('name').not().isEmpty() ],//normalize email turns BraedonB98@gmail.com into braedonb98@gmail.com
  usersController.signup)
 
 router.post('/login',
-[],
+[],//checks on input go here but none needed for login
  usersController.login)
 
 module.exports = router;
