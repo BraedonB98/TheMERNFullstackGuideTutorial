55a770f1ff4abb341c82b1f93f7770e310a9c149
braedonb98 <brbe3574@colorado.edu>
2022-01-04 16:43:02 -0700

    got it! issue with prop being missnamed when passed through users, to userlist, then to userItem

--- MainProject/FRONTEND/src/user/pages/Users.js
+++ MainProject/FRONTEND/src/user/pages/Users.js
@@ -14,6 +14,7 @@ const Users = () => {
       try{
         const responseData = await sendRequest('http://localhost:5000/api/users');
         setLoadedUsers(responseData.users);
+        console.log(responseData);
       }
       catch(error){}
     }
